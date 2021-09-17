0326d08bda3561bc2cb9454e97c9570f0b203bcc
samah mobasti <mobastisamah@gmail.com>
2021-09-17 09:44:27 +0100

    Router

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Second from './Components/Second';
+import './Components/Second';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Second/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Second.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import arch from './arch.PNG'
+
+
+function Second() {
+    return (
+        <div>
+      <div >
+          <h1 class="title red"> Your name</h1>
+          <br />
+          <img src={arch} alt="" />
+          <br />
+          <img src="/rfid.jpg"alt="" />
+      </div>
+ {/* <video src=""></video> */}
+        </div>
+    )
+}
+
+export default Second
+
+
